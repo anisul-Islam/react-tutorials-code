e896d6beebecd6f84c1443b300291012f5a583c0
Anisul Islam <anisul.islam@tuni.fi>
2020-12-06 08:45:07 +0200

    Introduction to JSX and JS Expression

--- src/components/JSX_JS_EXPRESSION/index.js
+++ src/components/JSX_JS_EXPRESSION/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 
-
 export default function JSX_JS_expression() {
-
     const todoTitle = "Call Family"
     const todoDesc = "Ipsum no sea sadipscing consetetur vero. Nonumy justo diam sed lorem sit, sed sit clita sit takimata sed sanctus invidunt."
     const fullDate = new Date();
     const date = fullDate.getDate() + "/" + fullDate.getMonth() + "/"+ fullDate.getFullYear();
-
     return (
         <div>
             <h1>Todo App</h1>
@@ -15,8 +12,7 @@ export default function JSX_JS_expression() {
             <p>{todoDesc}</p>
             <p>{date}</p>
             {/* <p>{fullDate.getDate() + "/" + fullDate.getMonth() + "/"+ fullDate.getFullYear()}</p> */}
-            {/* <p>{`${fullDate.getDate()}/${fullDate.getMonth()}/${fullDate.getFullYear()}`}</p> */}
-            
+            {/* <p>{`${fullDate.getDate()}/${fullDate.getMonth()}/${fullDate.getFullYear()}`}</p> */}            
         </div>
     )
 }
