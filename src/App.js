6a54f34e73ad59996b65a5feb7aa1726cf1bff85
Anisul Islam <anisul.islam@tuni.fi>
2020-12-06 08:42:46 +0200

    Component creation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import JSX_JS_EXPRESSION from './components/JSX_JS_EXPRESSION/index'
 import CSS_STYLE from './components/CSS_STYLE/index'
+import CARD from './components/CARD/index'
 
 export default function App() {
 
     return (
         <div>
             {/* <JSX_JS_EXPRESSION /> */}
-            <CSS_STYLE />
+            {/* <CSS_STYLE /> */}
+            <CARD />
         </div>
     )
 }
--- src/components/CARD/index.js
+++ src/components/CARD/index.js
@@ -1,37 +1,20 @@
 import React from 'react'
 import './style.css'
 
-export default function CSS_STYLE() {
+export default function CARD() {
 
     const todoTitle = "Call Family"
     const todoDesc = "Ipsum no sea sadipscing consetetur vero. Nonumy justo diam sed lorem sit, sed sit clita sit takimata sed sanctus invidunt."
     const fullDate = new Date();
     const date = fullDate.getDate() + "/" + fullDate.getMonth() + "/"+ fullDate.getFullYear();
 
-    /**
-     * CSS Example - creating JS Object of a CSS Style
-     */
-    const cardStyle = {
-        backgroundColor : 'palegreen', 
-        width: '350px', 
-        padding:'10px'
-    }
-
-
     return (
-
-
         <div>
-            
-            <h1 style={{backgroundColor : 'purple', textAlign : 'center', padding:'10px', color : 'white'}}>Todo App</h1> 
-
-             {/* Inline CSS Example  */}
-            <div style={cardStyle} className="card-style">
+            <div  className="card-style">
                 <h3>{todoTitle}</h3>
                 <p>{todoDesc}</p>
                 <p>{date}</p>
-            </div>
-            
+            </div>            
         </div>
     )
 }
