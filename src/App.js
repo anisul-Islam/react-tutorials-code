60dd955610795cbd620d6b2e04efd10c9d9a3028
anisul-islam <anisul2010s@yahoo.co.uk>
2020-12-23 02:33:36 +0200

    react hooks usestate

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React from 'react'
-import JSX_JS_EXPRESSION from './components/JSX_JS_EXPRESSION/index'
-import CSS_STYLE from './components/CSS_STYLE/index'
-import CARD from './components/CARD/index'
-import CLASS_COMPONENT from './components/CLASS_COMPONENT/index'
-import PROPS_DESTRUCTURING_FUNCTIONAL from './components/PROPS_DESTRUCTURING_FUNCTIONAL/index'
-import PROPS_DESTRUCTURING_CLASS from './components/PROPS_DESTRUCTURING_CLASS/index'
-import DATA_MAPPING from './components/DATA_MAPPING/index'
-import STATE_IN_CLASS from './components/STATE_IN_CLASS/index'
-import CONDITIONAL_RENDERING from './components/CONDITIONAL_RENDERING/index'
-import EVENT_HANDLING_CLASS from './components/EVENT_HANDLING_CLASS/index'
-import EVENT_HANDLING_FUNCTION from './components/EVENT_HANDLING_FUNCTION/index'
-import BINDING_EVENT_HANDLER from './components/BINDING_EVENT_HANDLER/index'
-import HOOKS_USESTATE from './components/HOOKS_USESTATE/index'
-import FORM1 from './components/FORM/index1'
-import FORM2 from './components/FORM/index2'
-import FORM3 from './components/FORM/index3'
-import FORM4 from './components/FORM/index4'
+import React from "react";
+import JSX_JS_EXPRESSION from "./components/JSX_JS_EXPRESSION/index";
+import CSS_STYLE from "./components/CSS_STYLE/index";
+import CARD from "./components/CARD/index";
+import CLASS_COMPONENT from "./components/CLASS_COMPONENT/index";
+import PROPS_DESTRUCTURING_FUNCTIONAL from "./components/PROPS_DESTRUCTURING_FUNCTIONAL/index";
+import PROPS_DESTRUCTURING_CLASS from "./components/PROPS_DESTRUCTURING_CLASS/index";
+import DATA_MAPPING from "./components/DATA_MAPPING/index";
+import STATE_IN_CLASS from "./components/STATE_IN_CLASS/index";
+import CONDITIONAL_RENDERING from "./components/CONDITIONAL_RENDERING/index";
+import EVENT_HANDLING_CLASS from "./components/EVENT_HANDLING_CLASS/index";
+import EVENT_HANDLING_FUNCTION from "./components/EVENT_HANDLING_FUNCTION/index";
+import BINDING_EVENT_HANDLER from "./components/BINDING_EVENT_HANDLER/index";
+import USESTATE from "./components/HOOKS/USESTATE/index";
+import FORM1 from "./components/FORM/index1";
+import FORM2 from "./components/FORM/index2";
+import FORM3 from "./components/FORM/index3";
+import FORM4 from "./components/FORM/index4";
+import USESTATE_PREVIOUS from "./components/HOOKS/USESTATE_PREVIOUS_STATE/index";
 
 export default function App() {
-
-    return (
-        <div>
-            {/* <JSX_JS_EXPRESSION /> */}
-            {/* <CSS_STYLE /> */}
-            {/* <CARD /> */}
-            {/* <CLASS_COMPONENT /> */}
-            {/* <PROPS_DESTRUCTURING_FUNCTIONAL title="Call Family" desc="Ipsum no sea sadipscing consetetur vero. Nonumy justo diam sed lorem sit"/> */}
-            {/* <PROPS_DESTRUCTURING_CLASS title="Call Family" desc="Ipsum no sea sadipscing consetetur vero. Nonumy justo diam sed lorem sit"/> */}
-            {/* <DATA_MAPPING /> */}
-            {/* <STATE_IN_CLASS /> */}
-            {/* <CONDITIONAL_RENDERING /> */}
-            {/* <EVENT_HANDLING_CLASS /> */}
-            {/* <EVENT_HANDLING_FUNCTION /> */}
-            {/* <BINDING_EVENT_HANDLER /> */}
-            {/* <HOOKS_USESTATE /> */}
-            <FORM4 />
-        </div>
-    )
+  return (
+    <div>
+      {/* <JSX_JS_EXPRESSION /> */}
+      {/* <CSS_STYLE /> */}
+      {/* <CARD /> */}
+      {/* <CLASS_COMPONENT /> */}
+      {/* <PROPS_DESTRUCTURING_FUNCTIONAL title="Call Family" desc="Ipsum no sea sadipscing consetetur vero. Nonumy justo diam sed lorem sit"/> */}
+      {/* <PROPS_DESTRUCTURING_CLASS title="Call Family" desc="Ipsum no sea sadipscing consetetur vero. Nonumy justo diam sed lorem sit"/> */}
+      {/* <DATA_MAPPING /> */}
+      {/* <STATE_IN_CLASS /> */}
+      {/* <CONDITIONAL_RENDERING /> */}
+      {/* <EVENT_HANDLING_CLASS /> */}
+      {/* <EVENT_HANDLING_FUNCTION /> */}
+      {/* <BINDING_EVENT_HANDLER /> */}
+      {/* <USESTATE /> */}
+      {/* <FORM4 /> */}
+    </div>
+  );
 }
