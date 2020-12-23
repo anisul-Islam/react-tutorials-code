659b8f941c71a4fc3210e1f1378037aad1e9680a
anisul-islam <anisul2010s@yahoo.co.uk>
2020-12-23 03:04:44 +0200

    react hooks usestate

--- src/components/HOOKS/USESTATE/index.js
+++ src/components/HOOKS/USESTATE/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function index() {
+export default function HOOKS_USESTATE() {
   const [count, setCount] = useState(0);
 
   const handleIncrement = () => {
