2fa0bbceb7ea9409d0c6d0d36a3f3b4e1c5470c4
anisul-islam <anisul2010s@yahoo.co.uk>
2022-03-20 21:56:44 +0200

    fragments

---
+++ src/components/Table/Column.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Column = () => {
+  return (
+    <>
+      <td>Anisul Islam</td>
+      <td>32 years old</td>
+    </>
+  );
+};
+
+export default Column;
---
+++ src/components/Table/Table.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Column from "./Column";
+
+const Table = () => {
+  return (
+    <table>
+      <tr>
+        <Column />
+      </tr>
+      <tr>
+        <Column />
+      </tr>
+    </table>
+  );
+};
+
+export default Table;
